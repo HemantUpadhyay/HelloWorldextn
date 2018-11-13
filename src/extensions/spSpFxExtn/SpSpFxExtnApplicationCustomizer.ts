866563427bd607a0d439a266d622605b80622f10
Hemant Upadhyay <hemantupadhyay59@gmail.com>
2018-11-13 19:11:14 +0530

    change in function to call azure function using $.ajax()

--- src/extensions/spSpFxExtn/SpSpFxExtnApplicationCustomizer.ts
+++ src/extensions/spSpFxExtn/SpSpFxExtnApplicationCustomizer.ts
@@ -4,9 +4,13 @@ import {
   BaseApplicationCustomizer
 } from '@microsoft/sp-application-base';
 import { Dialog } from '@microsoft/sp-dialog';
-
+import {
+  SPHttpClient,
+  SPHttpClientResponse ,
+  SPHttpClientConfiguration   
+ } from '@microsoft/sp-http';
 import * as strings from 'SpSpFxExtnApplicationCustomizerStrings';
-
+import * as $ from 'jquery'
 const LOG_SOURCE: string = 'SpSpFxExtnApplicationCustomizer';
 
 /**
@@ -25,22 +29,48 @@ export default class SpSpFxExtnApplicationCustomizer
 
   @override
   public onInit(): Promise<void> {
-    Dialog.alert(`before Initiate call.`)
+    //Dialog.alert("before Initiate call.");
     this.Initiate()
-    // Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
-    // let message: string = this.properties.testMessage;
-    // if (!message) {
-    //   message = '(No properties were provided.)';
-    // }
-    // Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
-    Dialog.alert(`after Initiate call.`)
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+    let message: string = this.properties.testMessage;
+    if (!message) {
+      message = '(No properties were provided.)';
+    }
+    //Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`);
+    //Dialog.alert("after Initiate call.")
     return Promise.resolve();
   }
 
+  private _getNewPageStatus():void {
+    let responseText: string = "";
+    var functionSIteIDUrl = "https://functesthelloworld.azurewebsites.net/api/HttpTrigger1?code=zWFUcRwMIeXtUaCCYP8BOWYFa5jQn5SAE9/hHqFL/6Uk/mfavUhw0Q==&name=Hemant";
+    this.context.spHttpClient.get(functionSIteIDUrl,SPHttpClient.configurations.v1)
+    .then((response: SPHttpClientResponse) => {
+      response.json().then((responseJSON: JSON) => {
+        responseText = JSON.stringify(responseJSON);
+            if (response.ok) {
+                //resultMsg.style.color = "green";
+                Dialog.alert("done");
+                console.log(response);
+            } else {
+                //resultMsg.style.color = "red";
+                Dialog.alert("fail");
+                console.log(response);
+            }
+      })
+           
+  }).catch((e) => {
+    console.log(e);
+  });
+}
+
+
+
   private async getNewPageStatus() {
-    const currentWebUrl: string = this.context.pageContext.web.absoluteUrl;
+    Dialog.alert("Inside getNewPageStatus function")
+    const currentWebUrl = this.context.pageContext.web.absoluteUrl;
     //const pageName = 'DynamicPage.aspx'
-    var functionSIteIDUrl : string = "https://functesthelloworld.azurewebsites.net/api/HttpTrigger1?code=zWFUcRwMIeXtUaCCYP8BOWYFa5jQn5SAE9/hHqFL/6Uk/mfavUhw0Q==&name=Hemant";
+    var functionSIteIDUrl = "https://functesthelloworld.azurewebsites.net/api/HttpTrigger1?code=zWFUcRwMIeXtUaCCYP8BOWYFa5jQn5SAE9/hHqFL/6Uk/mfavUhw0Q==&name=Hemant";
 
     const requestHeaders: Headers = new Headers();
     requestHeaders.append("Content-type", "application/jsonp");
@@ -55,36 +85,54 @@ export default class SpSpFxExtnApplicationCustomizer
 
     let responseText: string = "";
     let createPageStatus: string = "";
-    console.log("Wait started for Creating page");
-    await fetch(functionSIteIDUrl, postOptions).then((response) => {
-        console.log("Response returned");
-        if (response.ok) {
-          Dialog.alert(`Page status returned`);
-          return response.json()          
-        }
-        else
-        {
-            var errMsg = "Error detected while adding site page. Server response wasn't OK ";
-            console.log(errMsg);
-        } 
-      }).then((responseJSON: JSON) => {
-        responseText = JSON.stringify(responseJSON).trim();
-        console.log(responseText);
-        if(responseText.toLowerCase().indexOf("success") > 0)
-            {
-              console.log("success feedback");
-              //to make another call for next azure method on success of 1st method
-              this.insertWebPartToPage();
-            }
-        if(responseText.toLowerCase().indexOf("error") > 0)
-            {
-              console.log("web call errored");
-            }
+    console.log("Wait started for Creating page");    
+    $.ajax({
+      //JSONP API       
+      url: "https://functesthelloworld.azurewebsites.net/api/HttpTrigger1?code=zWFUcRwMIeXtUaCCYP8BOWYFa5jQn5SAE9/hHqFL/6Uk/mfavUhw0Q==&name=Hemant",
+      method: "GET",
+      dataType: "jsonp",
+      async:false,
+      headers: {'Content-Type':'application/json','Cache-Control':'private','Access-Control-Allow-Origin':'*'},
+      success: function (data) {
+        //alert('success');
+        console.log(data);
+      },
+      error: function (jqXHR, textStatus,error) {
+        console.log('error...!!!'+error)
       }
-    ).catch ((response: any) => {
-      let errMsg: string = `WARNING - error when calling URL ${functionSIteIDUrl}. Error = ${response.message}`;
-      console.log(errMsg);
     });
+    // await fetch(functionSIteIDUrl, postOptions).then((response) => {
+    //     console.log("Response returned");
+    //     if (response.ok) {
+    //       Dialog.alert(`Page status returned`);
+    //       return response.json()          
+    //     }
+    //     else
+    //     {
+    //         var errMsg = "Error detected while adding site page. Server response wasn't OK ";
+    //         console.log(errMsg);
+    //     } 
+    //   }).then((responseJSON: JSON) => {
+    //     responseText = JSON.stringify(responseJSON).trim();
+    //     console.log(responseText);
+    //     if(responseText.toLowerCase().indexOf("success") > 0)
+    //         {
+    //           console.log("success feedback");
+    //           //to make another call for next azure method on success of 1st method
+    //           this.insertWebPartToPage();
+    //         }
+    //     if(responseText.toLowerCase().indexOf("error") > 0)
+    //         {
+    //           console.log("web call errored");
+    //         }
+    //   }
+    // ).catch ((response: any) => {
+    //   let errMsg: string = `WARNING - error when calling URL ${functionSIteIDUrl}. Error = ${response.message}`;
+    //   console.log(errMsg);
+    // });
+
+
+
     console.log("wait finished");
   }
 
@@ -138,7 +186,8 @@ export default class SpSpFxExtnApplicationCustomizer
     console.log("wait for web Part add finished");
   }
   
-  private async Initiate() {
+  private async Initiate() {    
     await this.getNewPageStatus();
+    //this._getNewPageStatus();
   }
 }
